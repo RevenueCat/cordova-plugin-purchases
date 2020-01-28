d1a267f14864c08463cf4b2bc402d57e70804641
Andy Boedo <andresboedo@gmail.com>
2020-01-28 20:50:47 -0300

    updated the refresh and setup scripts

--- examples/cordova-sample/MyApp/bin/refresh_javascript_sources_ios.sh
+++ examples/cordova-sample/MyApp/bin/refresh_javascript_sources_ios.sh
@@ -5,10 +5,9 @@ SCRIPT_DIRECTORY="$(dirname "$(realpath "$0")")"
 cd $SCRIPT_DIRECTORY/../plugins/cordova-plugin-purchases
 npm run build
 
+# delete the file that stores the current version of the plugins so that cordova is forced to refresh
+rm $SRCROOT/ios.json
+
 # copy other js files through cordova
 cd $SRCROOT/../../
 cordova prepare ios
-
-# copy the plugin javascript file, since cordova prepare misses it
-cd $SCRIPT_DIRECTORY/../plugins/cordova-plugin-purchases
-cp www/plugin.js $SRCROOT/www/plugins/cordova-plugin-purchases/www/plugin.js
--- examples/cordova-sample/MyApp/bin/setup.sh
+++ examples/cordova-sample/MyApp/bin/setup.sh
@@ -1,6 +1,5 @@
 cordova plugin remove purchases
+cordova plugin add ../../../../cordova-plugin-purchases --link --save
 
 cordova platform add ios
 cordova platform add android
-
-cordova plugin add ../../../../cordova-plugin-purchases --link --save

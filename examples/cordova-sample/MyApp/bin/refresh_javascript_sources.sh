0c1977ff1329d13a39e8b432af05835fab9170b4
Andy Boedo <andresboedo@gmail.com>
2020-01-28 14:53:40 -0300

    added setup and refresh scripts

---
+++ examples/cordova-sample/MyApp/bin/refresh_javascript_sources.sh
@@ -0,0 +1,14 @@
+# in case this script is run from another directory, cd into the directory of the script
+SCRIPT_DIRECTORY="$(dirname "$(realpath "$0")")"
+
+# run the transpiling step
+cd $SCRIPT_DIRECTORY/../plugins/cordova-plugin-purchases
+npm run build
+
+# copy other js files through cordova
+cd $SRCROOT/../../
+cordova prepare ios
+
+# copy the plugin javascript file, since cordova prepare misses it
+cd $SCRIPT_DIRECTORY/../plugins/cordova-plugin-purchases
+cp ./www/plugin.js $SRCROOT/www/plugins/cordova-plugin-purchases/www/plugin.js
---
+++ examples/cordova-sample/MyApp/bin/setup.sh
@@ -0,0 +1,6 @@
+cordova plugin remove purchases
+
+cordova platform add ios
+cordova platform add android
+
+cordova plugin add ../../../../cordova-plugin-purchases --link --save

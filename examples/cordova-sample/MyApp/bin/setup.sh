9904939b3473a379066d287bc6242de81117c96d
Joshua Liebowitz <taquitos@users.noreply.github.com>
2022-06-24 13:06:24 -0700

    Optional API key for setup.bin (#132)
    Optional API key for setup

--- examples/cordova-sample/MyApp/bin/setup.sh
+++ examples/cordova-sample/MyApp/bin/setup.sh
@@ -1,4 +1,12 @@
 #! /bin/sh
+if [ -z "$1" ]
+  then
+    echo "No API Key supplied, use setup.sh <API_KEY> (without <>'s) if you wish to setup the project with an api key."
+  else
+  	API_KEY=$1
+	sed -i .bck s/api_key/$1/ www/js/index.js
+fi
+
 cordova platform remove ios
 cordova platform remove android
 

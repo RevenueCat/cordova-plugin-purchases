5c6dcc2472eda4b64ac6dc55296d8a229f8ae255
Joshua Liebowitz <josh@revenuecat.com>
2022-06-24 09:56:45 -0700

    Update setup.sh
    Make it optional

--- examples/cordova-sample/MyApp/bin/setup.sh
+++ examples/cordova-sample/MyApp/bin/setup.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
 if [ -z "$1" ]
   then
-    echo "No API Key supplied, use setup.sh <API_KEY> (without <>'s)"
-    exit 1
+    echo "No API Key supplied, use setup.sh <API_KEY> (without <>'s) if you wish to setup the project with an api key."
+  else
+  	API_KEY=$1
+	sed -i .bck s/api_key/$1/ www/js/index.js
 fi
-
-API_KEY=$1
-sed -i .bck s/api_key/$1/ www/js/index.js
 
 cordova platform remove ios
 cordova platform remove android

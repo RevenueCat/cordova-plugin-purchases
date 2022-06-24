898a3120230323474c7f2fd9750fe94ca60b03fc
Joshua Liebowitz <josh@revenuecat.com>
2022-06-24 09:54:27 -0700

    Require API key for setup

--- examples/cordova-sample/MyApp/bin/setup.sh
+++ examples/cordova-sample/MyApp/bin/setup.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
+if [ -z "$1" ]
+  then
+    echo "No API Key supplied, use setup.sh <API_KEY> (without <>'s)"
+    exit 1
+fi
+
+API_KEY=$1
+sed -i .bck s/api_key/$1/ www/js/index.js
+
 cordova platform remove ios
 cordova platform remove android
 

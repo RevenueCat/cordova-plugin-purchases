4559610332e99fc807d2919c697775935f3ec77f
Joshua Liebowitz <josh@revenuecat.com>
2022-06-29 16:52:28 -0700

    Add "Refresh js resources" build phase run script
    Resolves [CSDK-271]

--- examples/cordova-sample/MyApp/bin/sk_config_setup.rb
+++ examples/cordova-sample/MyApp/bin/sk_config_setup.rb
@@ -1,6 +1,9 @@
-# This script adds the StoreKit config file into the debug scheme
-# that way everytime the project is regenerated, we don't have to
-# reconfigure the workspace each time.
+# This script adds:
+# StoreKit config file into the debug scheme and project.
+# refresh_javascript_sources_ios.sh as a build phase run script.
+# This way every time the project is regenerated we don't have to
+# reconfigure the workspace.
+
 require 'xcodeproj'
 
 workspace_path = 'platforms/ios/HelloCordova.xcworkspace'
@@ -15,6 +18,20 @@ storekit_file_path_for_xcscheme = "../#{storekit_file_path_for_xcproject}"
 puts("Adding StoreKit Config to project")
 project = Xcodeproj::Project.open(project_path)
 project.new_file(storekit_file_path_for_xcproject)
+
+run_script_name = 'Refresh js resources'
+puts("Adding '#{run_script_name}' run script to all targets:")
+for target in project.targets 
+    phase = target.shell_script_build_phases().find {|item| item.name == run_script_name}
+    if (phase.nil?)
+        phase = target.new_shell_script_build_phase(run_script_name)
+        phase.shell_script = "$PROJECT_DIR/../../bin/refresh_javascript_sources_ios.sh"
+        puts("Added to target: #{target.name}")
+    else
+        puts "Ignored target '#{target.name}', script already existed"
+    end
+end
+
 project.save
 
 shared_data_dir = Xcodeproj::XCScheme.shared_data_dir(workspace_path)

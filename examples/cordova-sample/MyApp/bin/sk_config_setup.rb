c8dcac3a57e8ec411f9bd06598bfc1f07ae39f4e
Joshua Liebowitz <josh@revenuecat.com>
2022-06-30 15:00:02 -0700

    Update project setup script and Swift to 5.0
    The script needed to be updated to handle situations where the Swift version didn't get set.
    Setting version to 5 showed that there were still a couple more updates needed for the Swift file.

--- examples/cordova-sample/MyApp/bin/sk_config_setup.rb
+++ examples/cordova-sample/MyApp/bin/sk_config_setup.rb
@@ -15,9 +15,20 @@ puts("Project: #{project_path}\n")
 storekit_file_path_for_xcproject = '../../RevenueCatTest.storekit'
 storekit_file_path_for_xcscheme = "../#{storekit_file_path_for_xcproject}"
 
-puts("Adding StoreKit Config to project")
+puts("Checking if we need to add the StoreKit Config to xcproject")
 project = Xcodeproj::Project.open(project_path)
-project.new_file(storekit_file_path_for_xcproject)
+already_exists = false
+for file in project.files
+    if file.path == storekit_file_path_for_xcproject
+        puts("StoreKit Config already exists in the project")
+        already_exists = true
+        break
+    end
+end
+if not already_exists 
+    puts("Adding StoreKit Config to project")
+    project.new_file(storekit_file_path_for_xcproject)
+end
 
 run_script_name = 'Refresh js resources'
 puts("Adding '#{run_script_name}' run script to all targets:")
@@ -31,6 +42,20 @@ for target in project.targets
         puts "Ignored target '#{target.name}', script already existed"
     end
 end
+
+
+swift_version_to_set = "5.0"
+puts("Updating build configuration if they are missing Swift version")
+for build_config in project.build_configurations
+    swift_version = build_config.build_settings["SWIFT_VERSION"]
+    if (swift_version.nil?)
+            puts("Setting version #{swift_version_to_set}")
+        build_config.build_settings["SWIFT_VERSION"] = swift_version_to_set
+    else
+        puts("Version #{swift_version} already existed, leaving as-is")
+    end
+end
+
 
 project.save
 

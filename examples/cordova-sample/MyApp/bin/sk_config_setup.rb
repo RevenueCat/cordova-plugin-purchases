2ad4d02ddd36c548b4e423d0894bbe84f87ac629
Joshua Liebowitz <josh@revenuecat.com>
2022-07-01 10:36:22 -0700

    Update sk_config_setup.rb
    Reorganized and broke into methods

--- examples/cordova-sample/MyApp/bin/sk_config_setup.rb
+++ examples/cordova-sample/MyApp/bin/sk_config_setup.rb
@@ -6,77 +6,103 @@
 
 require 'xcodeproj'
 
+def save_project_if_needed(project, modified = false)
+    unless modified
+        return
+    end
+
+    puts("Saving project")
+    project.save
+end
+
+def add_store_kit_to_project_if_needed(project, storekit_file_path)
+    puts("Checking if StoreKit Config exists in #{project.path}")
+    modified = false
+    already_exists = project.files.any? { |file| file.path == storekit_file_path }
+    if not already_exists 
+        puts(" - Missing, adding.")
+        project.new_file(storekit_file_path)
+        modified = true
+    else 
+        puts(" - Exists, skipping.")
+    end
+    return modified
+end
+
+def add_build_phase_run_script_if_needed(project, run_script_name = 'Refresh js resources')
+    puts("Checking '#{run_script_name}' run script exists in all targets")
+    modified = false
+    for target in project.targets 
+        phase = target.shell_script_build_phases().find {|item| item.name == run_script_name}
+        if (phase.nil?)
+            phase = target.new_shell_script_build_phase(run_script_name)
+            phase.shell_script = "$PROJECT_DIR/../../bin/refresh_javascript_sources_ios.sh"
+            puts(" - Missing in #{target.name}, adding.")
+            modified = true
+        else
+            puts " - Exists in '#{target.name}', skipping."
+        end
+    end
+    return modified
+end
+
+def add_swift_version_if_needed(project, swift_version_to_set = "5.0")
+    puts("Build configurations missing Swift version?")
+    modified = false
+    for build_config in project.build_configurations
+        swift_version = build_config.build_settings["SWIFT_VERSION"]
+        if (swift_version.nil?)
+            puts(" - #{build_config.name} missing, setting version #{swift_version_to_set}.")
+            build_config.build_settings["SWIFT_VERSION"] = swift_version_to_set
+            modified = true
+        else
+            puts(" - #{build_config.name} already set to #{swift_version}, skipping.")
+        end
+    end
+    return modified
+end
+
+def update_scheme_with_store_kit_file_if_needed(workspace_path, storekit_file_path_for_xcproject)
+    shared_data_dir = Xcodeproj::XCScheme.shared_data_dir(workspace_path)
+    scheme_filename = "HelloCordova.xcscheme"
+    scheme_path = File.join(shared_data_dir, scheme_filename)
+
+    sk_node_label = 'StoreKitConfigurationFileReference'
+    puts("Checking if Scheme: #{scheme_path} needs #{sk_node_label} added")
+    scheme = Xcodeproj::XCScheme.new File.join(shared_data_dir, scheme_filename)
+
+    # check if element exists
+    unless scheme.launch_action.xml_element.elements[sk_node_label].nil?
+        puts(" - No, StoreKitConfigurationFileReference already exists")
+        return
+    end
+
+    storekit_file_path_for_xcscheme = "../#{storekit_file_path_for_xcproject}"
+
+    sk_config = REXML::Element.new(sk_node_label)
+    puts(" - Yes, creating: #{sk_node_label} node and adding `identifier` attribute: #{storekit_file_path_for_xcscheme}")
+    sk_config.attributes['identifier'] = storekit_file_path_for_xcscheme
+    scheme.launch_action.xml_element.add_element(sk_config)
+
+    puts("Saving scheme")
+    scheme.save!
+end
+
+# Script start
 workspace_path = 'platforms/ios/HelloCordova.xcworkspace'
 puts("Workspace: #{workspace_path}\n")
 
 project_path = 'platforms/ios/HelloCordova.xcodeproj'
-puts("Project: #{project_path}\n")
+project = Xcodeproj::Project.open(project_path)
+puts("Project: #{project.path}\n")
 
 storekit_file_path_for_xcproject = '../../RevenueCatTest.storekit'
-storekit_file_path_for_xcscheme = "../#{storekit_file_path_for_xcproject}"
 
-puts("Checking if we need to add the StoreKit Config to xcproject")
-project = Xcodeproj::Project.open(project_path)
-already_exists = false
-for file in project.files
-    if file.path == storekit_file_path_for_xcproject
-        puts("StoreKit Config already exists in the project")
-        already_exists = true
-        break
-    end
-end
-if not already_exists 
-    puts("Adding StoreKit Config to project")
-    project.new_file(storekit_file_path_for_xcproject)
-end
 
-run_script_name = 'Refresh js resources'
-puts("Adding '#{run_script_name}' run script to all targets:")
-for target in project.targets 
-    phase = target.shell_script_build_phases().find {|item| item.name == run_script_name}
-    if (phase.nil?)
-        phase = target.new_shell_script_build_phase(run_script_name)
-        phase.shell_script = "$PROJECT_DIR/../../bin/refresh_javascript_sources_ios.sh"
-        puts("Added to target: #{target.name}")
-    else
-        puts "Ignored target '#{target.name}', script already existed"
-    end
-end
+modified = add_store_kit_to_project_if_needed(project=project, storekit_file_path_for_xcproject)
+modified = add_build_phase_run_script_if_needed(project) || modified
+modified = add_swift_version_if_needed(project, save=true) || modified
+save_project_if_needed(project, modified)
+update_scheme_with_store_kit_file_if_needed(workspace_path, storekit_file_path_for_xcproject)
 
-swift_version_to_set = "5.0"
-puts("Updating build configuration if they are missing Swift version")
-for build_config in project.build_configurations
-    swift_version = build_config.build_settings["SWIFT_VERSION"]
-    if (swift_version.nil?)
-            puts("Setting version #{swift_version_to_set}")
-        build_config.build_settings["SWIFT_VERSION"] = swift_version_to_set
-    else
-        puts("Version #{swift_version} already existed, leaving as-is")
-    end
-end
-
-project.save
-
-shared_data_dir = Xcodeproj::XCScheme.shared_data_dir(workspace_path)
-scheme_filename = "HelloCordova.xcscheme"
-scheme_path = File.join(shared_data_dir, scheme_filename)
-
-puts("Using Scheme: #{scheme_path}")
-scheme = Xcodeproj::XCScheme.new File.join(shared_data_dir, scheme_filename)
-
-# check if element exists
-sk_node_label = 'StoreKitConfigurationFileReference'
-unless scheme.launch_action.xml_element.elements[sk_node_label].nil?
-	puts("No need to modify Scheme, StoreKitConfigurationFileReference already exists")
-	exit
-end
-
-sk_config = REXML::Element.new(sk_node_label)
-puts("Creating: #{sk_node_label} node and adding `identifier` attribute: #{storekit_file_path_for_xcscheme}")
-sk_config.attributes['identifier'] = storekit_file_path_for_xcscheme
-scheme.launch_action.xml_element.add_element(sk_config)
-
-puts("Saving scheme")
-scheme.save!
-
-puts("Done!")
+puts("Done! 🎉")

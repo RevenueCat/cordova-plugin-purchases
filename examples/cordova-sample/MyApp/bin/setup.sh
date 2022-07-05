9e7892169a28ade216ca836234c155f92a3401ae
Joshua Liebowitz <josh@revenuecat.com>
2022-07-05 16:44:24 -0700

    Add ability to only setup a single platform for sample app

--- examples/cordova-sample/MyApp/bin/setup.sh
+++ examples/cordova-sample/MyApp/bin/setup.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+START_TIME=`date +%s`
 command -v bundle >/dev/null 2>&1 || { echo >&2 "bundler is not installed. run gem install bundler."; exit 1; }
 OG_DIR=$PWD
 
@@ -14,19 +15,49 @@ if [ -z "$1" ]
 	sed -i .bck s/api_key/$1/ www/js/index.js
 fi
 
-cordova platform remove ios
-cordova platform remove android
+if [ $# -eq 2 ]
+then
+  PLATFORM=$2
+else
+  PLATFORM="all"
+fi
 
-cordova platform add ios
-cordova platform add android
+common_configure() {
+  if [ $PLATFORM == "ios" ]; then
+    cordova platform add ios
+  elif [ $PLATFORM == "android" ]; then
+    cordova platform add android
+  else
+    cordova platform add ios
+    cordova platform add android
+  fi
 
-cordova plugin remove cordova-plugin-purchases
-cordova plugin add ../../../ --link --save
+  cordova plugin remove cordova-plugin-purchases
+  cordova plugin add ../../../ --link --save
 
-cd ../../../
-npm install
+  cd ../../../
+  npm install
 
-cd $OG_DIR
-SK_CONFIG_SCRIPT_PATH=bin/sk_config_setup.rb
-echo "Running ${SK_CONFIG_SCRIPT_PATH}"
-bundle exec ruby $SK_CONFIG_SCRIPT_PATH
+  if [[ $PLATFORM == "ios" || $PLATFORM == "all" ]]; then
+    cd $OG_DIR
+    SK_CONFIG_SCRIPT_PATH=bin/sk_config_setup.rb
+    echo "Running ${SK_CONFIG_SCRIPT_PATH}"
+    bundle exec ruby $SK_CONFIG_SCRIPT_PATH
+  fi
+
+}
+
+configure() {
+  echo "Configuring for platform:" $PLATFORM
+
+  cordova platform remove ios
+  cordova platform remove android
+
+  common_configure
+}
+
+configure
+
+END_TIME=`date +%s`
+RUNTIME=$((END_TIME-START_TIME))
+echo "Setup took" $RUNTIME "seconds"

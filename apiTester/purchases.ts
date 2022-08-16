66b2f996ada1baaed9a3a852354fe75d87638403
Joshua Liebowitz <taquitos@users.noreply.github.com>
2022-08-16 15:13:31 -0700

    Add APITester (#169)
    Resolves [CSDK-142]
    
    Adds api tester.
    Use the docker image for ruby since that seems to work for other jobs.
    Reduce dependencies in ci for API Tester

---
+++ apiTester/purchases.ts
@@ -0,0 +1,93 @@
+import {
+  CustomerInfo,
+  IntroEligibility,
+  LogInResult,
+  PurchasesError,
+  PurchasesOfferings,
+  PurchasesPackage,
+  PurchasesPromotionalOffer,
+  PurchasesStoreProduct,
+  UpgradeInfo,
+  PURCHASE_TYPE, 
+  PurchasesStoreProductDiscount, 
+  BILLING_FEATURE,
+  ShouldPurchasePromoProductListener,
+} from '../www/plugin';
+
+import Purchases from '../www/plugin';
+
+const errorCallback = (error: PurchasesError) => {};
+
+function checkPurchases(purchases: Purchases) {
+  const productIds: string[] = [];
+
+  Purchases.getOfferings(offerings => { const offeringsData: PurchasesOfferings = offerings; }, errorCallback);
+  Purchases.getProducts(productIds, products => { const productsData: PurchasesStoreProduct[] = products; }, errorCallback);
+  Purchases.restorePurchases(callback => { const customerInfo: CustomerInfo = callback; }, errorCallback);
+}
+
+function checkUsers(purchases: Purchases) {
+  Purchases.getAppUserID(appUserID => { const userID: string = appUserID; });
+  Purchases.logIn("userId", loginResult => { const login: LogInResult = loginResult; }, errorCallback);
+  Purchases.logOut(callback => { const customerInfo: CustomerInfo = callback; }, errorCallback);
+  Purchases.getCustomerInfo(callback => { const customerInfo: CustomerInfo = callback; }, errorCallback);
+  Purchases.isAnonymous(callback => { const isAnonymous: boolean = callback; });
+}
+
+function checkPurchasing(purchases: Purchases,
+                         product: PurchasesStoreProduct,
+                         discount: PurchasesStoreProductDiscount,
+                         paymentDiscount: PurchasesPromotionalOffer,
+                         pack: PurchasesPackage) {
+  const productId: string = "";
+  const upgradeInfo: UpgradeInfo | null = null;
+  const features: BILLING_FEATURE[] = [];
+  
+  const purchaseCallback = ({productIdentifier, customerInfo,}: { productIdentifier: string; customerInfo: CustomerInfo; }) => {};
+  const errorCallbackUserCancelled = ({error, userCancelled,}: { error: PurchasesError; userCancelled: boolean; }) => {};
+
+  Purchases.purchaseProduct(productId, purchaseCallback, errorCallbackUserCancelled, upgradeInfo, PURCHASE_TYPE.INAPP);
+  Purchases.purchasePackage(pack, purchaseCallback, errorCallbackUserCancelled, upgradeInfo);
+  Purchases.syncPurchases();
+
+  Purchases.canMakePayments(features, callback => { const canMakePayments: boolean = callback; }, errorCallback);
+  Purchases.getOfferings(offerings => { const offeringsData: PurchasesOfferings = offerings; }, errorCallback);
+
+  const eligibilityCallback = (map: { [productId:string]:IntroEligibility; }) => {};
+  Purchases.checkTrialOrIntroductoryPriceEligibility([""], eligibilityCallback);
+}
+
+function checkConfigure() {
+  const apiKey: string = "";
+  const appUserID: string | null = "";
+  const observerMode: boolean = false;
+  const userDefaultsSuiteName: string = "";
+
+  Purchases.configure(
+    apiKey,
+    appUserID,
+    observerMode
+  );
+  Purchases.configure(
+    apiKey,
+    appUserID,
+    observerMode,
+    userDefaultsSuiteName
+  );
+
+  Purchases.setProxyURL("");
+  Purchases.setDebugLogsEnabled(true);
+  Purchases.setSimulatesAskToBuyInSandbox(true);
+}
+
+function checkMisc() {
+  Purchases.presentCodeRedemptionSheet();
+  Purchases.invalidateCustomerInfoCache();
+}
+
+function checkListeners() {
+  const shouldPurchaseListener: ShouldPurchasePromoProductListener = deferredPurchase => {};
+
+  Purchases.addShouldPurchasePromoProductListener(shouldPurchaseListener);
+  Purchases.removeShouldPurchasePromoProductListener(shouldPurchaseListener);
+}

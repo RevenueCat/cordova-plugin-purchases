9168e62ddc0a379b5b53dd60a2e34dea3182a48c
Joshua Liebowitz <josh@revenuecat.com>
2022-08-16 12:53:04 -0700

    APITester
    Adds api tester.
    CI configure as well.
    Resolves [CSDK-142]

---
+++ apiTester/attributes.ts
@@ -0,0 +1,28 @@
+import Purchases from "../www/plugin";
+
+function checkAttributes(purchases: Purchases) {
+  const attributes: { [key: string]: string | null } = {};
+  const stringOrNull: string | null = "";
+
+  Purchases.setAttributes(attributes);
+  Purchases.setEmail(stringOrNull);
+  Purchases.setPhoneNumber(stringOrNull);
+  Purchases.setDisplayName(stringOrNull);
+  Purchases.setPushToken(stringOrNull);
+  Purchases.setAdjustID(stringOrNull);
+  Purchases.setAppsflyerID(stringOrNull);
+  Purchases.setFBAnonymousID(stringOrNull);
+  Purchases.setMparticleID(stringOrNull);
+  Purchases.setOnesignalID(stringOrNull);
+  Purchases.setAirshipChannelID(stringOrNull);
+  Purchases.setMediaSource(stringOrNull);
+  Purchases.setCampaign(stringOrNull);
+  Purchases.setAdGroup(stringOrNull);
+  Purchases.setAd(stringOrNull);
+  Purchases.setKeyword(stringOrNull);
+  Purchases.setCreative(stringOrNull);
+
+  Purchases.collectDeviceIdentifiers();
+  Purchases.setAutomaticAppleSearchAdsAttributionCollection(true);
+  Purchases.enableAdServicesAttributionTokenCollection();
+}

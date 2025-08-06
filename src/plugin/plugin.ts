a720f795ee47a6d5553caa2b6d9398c3ed95afad
Will Taylor <wtaylor151@gmail.com>
2025-08-06 11:14:49 -0500

    add VC models

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1021,6 +1021,42 @@ export enum PERIOD_UNIT {
   UNKNOWN = "UNKNOWN",
 }
 
+/**
+ * The VirtualCurrencies object contains all the virtual currencies associated to the user.
+ */
+export interface PurchasesVirtualCurrencies {
+  /**
+   * Map of all `PurchasesVirtualCurrency` objects keyed by virtual currency code.
+   */
+  readonly all: { [key: string]: PurchasesVirtualCurrency };
+}
+
+/**
+ * The VirtualCurrency object represents information about a virtual currency in the app.
+ * Use this object to access information about a virtual currency, such as its current balance.
+ */
+export interface PurchasesVirtualCurrency {
+  /**
+   * The virtual currency's balance.
+   */
+  readonly balance: number;
+
+  /**
+   * The virtual currency's name.
+   */
+  readonly name: string;
+
+  /**
+   * The virtual currency's code.
+   */
+  readonly code: string;
+
+  /**
+   * The virtual currency's description defined in the RevenueCat dashboard.
+   */
+  readonly serverDescription: string | null;
+}
+
 export type ShouldPurchasePromoProductListener = (deferredPurchase: () => void) => void;
 let shouldPurchasePromoProductListeners: ShouldPurchasePromoProductListener[] = [];
 

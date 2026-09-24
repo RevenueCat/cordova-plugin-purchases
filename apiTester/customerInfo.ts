efb69346195cc6f9a17f9516fe9c29d31e9b8121
Álvaro Brey <alvaro.brey@revenuecat.com>
2026-09-24 13:16:55 +0200

    Type PurchasesEntitlementInfo.periodType as PeriodType

--- apiTester/customerInfo.ts
+++ apiTester/customerInfo.ts
@@ -3,6 +3,7 @@ import {
   CustomerInfo,
   PurchasesEntitlementInfo,
   PurchasesEntitlementInfos,
+  PeriodType,
   PurchasesStoreTransaction
 } from "../www/plugin";
 
@@ -36,7 +37,7 @@ function checkEntitlementInfo(info: PurchasesEntitlementInfo) {
   const identifier: string = info.identifier;
   const isActive: boolean = info.isActive;
   const willRenew: boolean = info.willRenew;
-  const periodType: string = info.periodType;
+  const periodType: PeriodType = info.periodType;
   const latestPurchaseDate: string = info.latestPurchaseDate;
   const originalPurchaseDate: string = info.originalPurchaseDate;
   const expirationDate: string | null = info.expirationDate;
